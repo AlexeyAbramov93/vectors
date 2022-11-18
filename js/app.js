9dd3012665e2a1c353a40c99714300a2c41a9e5b
Alexey Abramov <alexey.abramov93@gmail.com>
2022-11-19 02:36:59 +0300

    Работаю над структурой кода

--- js/app.js
+++ js/app.js
@@ -1,24 +1,65 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-let isOrangeCaptured = false; // true if the orange line is captured onmousedown, otherwise false
-let isGreenCaptured = false; // true if the orange line is captured onmousedown, otherwise false
-let isRedCaptured = false; // true if the orange line is captured onmousedown, otherwise false
 
-let isOrangeLighted = false; // true if the orange line is captured onmousemove, otherwise false
-let isGreenLighted = false; // true if the orange line is captured onmousemove, otherwise false
-let isRedLighted = false; // true if the orange line is captured onmousemove, otherwise false
+axisScale = 0.9;    // to set scale of the canvas
+
+// shifted origin of the canvas geometic center 
+o = {
+    x: canvas.width*0.75,
+    y: canvas.height*0.25,
+}
+
+// to draw axes from the canvas geometic center 
+function drawAxes(width=0.3){
+    ctx.save();
+    ctx.lineWidth = width;
+    ctx.strokeStyle = 'black';
+    arc_count=4;        // to set a number of arcs
+    for (let i=0;i<arc_count;i++){
+        ctx.beginPath();
+        ctx.arc(o.x,o.y,(0.25*canvas.width/arc_count*(i+1))*axisScale,0,Math.PI*2);
+        ctx.stroke();
+    }
+
+    degrees={0:'0',1:'-30',2:'-60',3:'-90',4:'-120',5:'-150',6:'-180',7:'150',8:'120',9:'90',10:'60',11:'30'};
+    for (let i=0;i<12;i++){
+        ctx.beginPath();
+        ctx.moveTo(o.x,o.y);
+        ctx.lineTo(o.x,o.y*(1-axisScale));
+        ctx.font = "16px serif";
+        ctx.textAlign = "center";
+        ctx.fillText(degrees[i], o.x, o.y*(0.98-axisScale));
+        ctx.stroke();
+        ctx.translate(o.x, o.y);            // translate to rectangle center
+        ctx.rotate((Math.PI / 180) * 30);   // rotate
+        ctx.translate(-o.x, -o.y);          // translate back
+    }
+    ctx.restore();
+}
 
 class Vector {
+
     xPrev;
     yPrev;
+    
+    isLighted = false; // true if vector is captured onmousemove, otherwise false
+    isCaptured = false; // true if the vector is captured onmousedown, otherwise false
 
     constructor(xPrev, yPrev) {
       this.xPrev = xPrev;
       this.yPrev = yPrev;
     }
 
-    draw (x1,y1,color,width=3){
+    radius() {  // to calculate distance from center to vector point
+        return (((this.xPrev-o.x)**2 + (this.yPrev-o.y)**2)**0.5/200/axisScale).toFixed(2);
+    }
+
+    clickDistance(x,y) { // to calculate distance from click point to vector point
+        return ((this.xPrev-x)**2+(this.yPrev-y)**2)**0.5;
+    }
+
+    draw (x1,y1,color,width=3){ // to draw vector from center to point(x1,y1)
         ctx.save();
         ctx.lineCap = 'round';
         ctx.lineWidth = width;
@@ -54,85 +95,6 @@ class Vector {
     }
 }
 
-
-// shifted origin of the canvas geometic center 
-o = {
-    x: canvas.width/2,
-    y: canvas.height/2,
-}
-
-// to draw axes from the canvas geometic center 
-function drawAxes(width=0.3){
-    ctx.save();
-    ctx.lineWidth = width;
-    ctx.strokeStyle = 'black';
-    arc_scale = 0.9;    // to set scale the biggest arc from 0 to 1
-    arc_count=4;        // to set a number of arcs
-    for (let i=0;i<arc_count;i++){
-        ctx.beginPath();
-        ctx.arc(o.x,o.y,(o.x/arc_count*(i+1))*arc_scale,0,Math.PI*2);
-        ctx.stroke();
-    }
-
-    degrees={0:'0',1:'-30',2:'-60',3:'-90',4:'-120',5:'-150',6:'-180',7:'150',8:'120',9:'90',10:'60',11:'30'};
-    for (let i=0;i<12;i++){
-        ctx.beginPath();
-        ctx.moveTo(o.x,o.y);
-        ctx.lineTo(o.x,o.y*0.1);
-        ctx.textAlign = "center";
-        ctx.fillText(degrees[i], o.x, o.y*0.09);
-        ctx.stroke();
-        ctx.translate(o.x, o.y);            // translate to rectangle center
-        ctx.rotate((Math.PI / 180) * 30);   // rotate
-        ctx.translate(-o.x, -o.y);          // translate back
-    }
-    ctx.restore();
-}
-
-// 
-myLine = {
-    x1Prev: canvas.width/2,
-    y1Prev: canvas.height/5,
-
-    draw: function(x1,y1,color='black',width=3){
-        ctx.save();
-
-        ctx.lineCap = 'round';
-        ctx.lineWidth = width;
-        ctx.strokeStyle = color;
-        // draw line
-        ctx.beginPath();
-        ctx.moveTo(o.x,o.y);
-        ctx.lineTo(x1,y1);
-        ctx.stroke();
-        
-        arrow_div = 1.15;
-        arrow_angle = 3;
-        // draw the first arrow half
-        ctx.beginPath();
-        ctx.translate(o.x, o.y);    // translate to rectangle center
-        ctx.moveTo((x1-o.x)/arrow_div,(y1-o.y)/arrow_div);
-        ctx.rotate((Math.PI / 180) * arrow_angle);  // rotate
-        ctx.moveTo((x1-o.x)/arrow_div,(y1-o.y)/arrow_div);
-        ctx.rotate((Math.PI / 180) * -arrow_angle);  // rotate
-        ctx.lineTo(x1-o.x,y1-o.y);
-        ctx.stroke();
-
-        // draw the second arrow half
-        ctx.beginPath();
-        ctx.moveTo((x1-o.x)/arrow_div,(y1-o.y)/arrow_div);
-        ctx.rotate((Math.PI / 180) * -arrow_angle);  // rotate
-        ctx.moveTo((x1-o.x)/arrow_div,(y1-o.y)/arrow_div);
-        ctx.rotate((Math.PI / 180) * arrow_angle);  // rotate
-        ctx.lineTo(x1-o.x,y1-o.y);
-        ctx.stroke();
-
-        ctx.restore();
-    }
-}
-
-
-
 // to calculate coordinates of the turned point
 // https://foxford.ru/wiki/informatika/povorot-tochki
 function calcCoordinatesOfTurnedPoint(curPointX, curPointY, oX, oY, degree){
@@ -141,8 +103,10 @@ function calcCoordinatesOfTurnedPoint(curPointX, curPointY, oX, oY, degree){
     return {'x': turnedPointX, 'y': turnedPointY};
 }
 
-let initXOrange = canvas.width/2;
-let initYOrange = canvas.height/5;
+let initXOrange = o.x;
+let initYOrange = (o.y*0.25)/axisScale;
+console.log(`o.x:${o.x} o.y: ${o.y}`);
+console.log(`initXOrange:${initXOrange} initYOrange: ${initYOrange}`);
 let vectorOrange = new Vector(initXOrange,initYOrange);
 let initXGreen=calcCoordinatesOfTurnedPoint(initXOrange, initYOrange, o.x, o.y, 120)['x'];
 let initYGreen=calcCoordinatesOfTurnedPoint(initXOrange, initYOrange, o.x, o.y, 120)['y'];
@@ -157,7 +121,7 @@ directSequence = {
     draw: function(x1,y1,width){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawAxes();
-        if (isGreenCaptured || isGreenLighted){
+        if (vectorGreen.isCaptured || vectorGreen.isLighted){
             vectorGreen.draw(x1,y1,'green',width=width)
             x2 = calcCoordinatesOfTurnedPoint(x1, y1, o.x, o.y, 120)['x'];
             y2 = calcCoordinatesOfTurnedPoint(x1, y1, o.x, o.y, 120)['y'];
@@ -171,7 +135,7 @@ directSequence = {
             vectorRed.yPrev = calcCoordinatesOfTurnedPoint(x1, y1, o.x, o.y, 120)['y'];
             vectorOrange.xPrev = calcCoordinatesOfTurnedPoint(x1, y1, o.x, o.y, -120)['x'];
             vectorOrange.yPrev = calcCoordinatesOfTurnedPoint(x1, y1, o.x, o.y, -120)['y'];
-        } else if (isRedCaptured || isRedLighted){
+        } else if (vectorRed.isCaptured || vectorRed.isLighted){
             vectorRed.draw(x1,y1,'red',width=width)
             x2 = calcCoordinatesOfTurnedPoint(x1, y1, o.x, o.y, 120)['x'];
             y2 = calcCoordinatesOfTurnedPoint(x1, y1, o.x, o.y, 120)['y'];
@@ -185,7 +149,7 @@ directSequence = {
             vectorOrange.yPrev = calcCoordinatesOfTurnedPoint(x1, y1, o.x, o.y, 120)['y'];
             vectorGreen.xPrev = calcCoordinatesOfTurnedPoint(x1, y1, o.x, o.y, -120)['x'];
             vectorGreen.yPrev = calcCoordinatesOfTurnedPoint(x1, y1, o.x, o.y, -120)['y'];
-        } else if (isOrangeCaptured || isOrangeLighted){
+        } else if (vectorOrange.isCaptured || vectorOrange.isLighted){
             vectorOrange.draw(x1,y1,'orange',width=width)
             x2 = calcCoordinatesOfTurnedPoint(x1, y1, o.x, o.y, 120)['x'];
             y2 = calcCoordinatesOfTurnedPoint(x1, y1, o.x, o.y, 120)['y'];
@@ -206,88 +170,88 @@ directSequence = {
 canvas.onmousedown = function(event){
     var x = event.offsetX;
     var y = event.offsetY;
-
-    var orangeClickDistance = ((vectorOrange.xPrev-x)**2+(vectorOrange.yPrev-y)**2)**0.5;
-    var greenClickDistance = ((vectorGreen.xPrev-x)**2+(vectorGreen.yPrev-y)**2)**0.5;
-    var redClickDistance = ((vectorRed.xPrev-x)**2+(vectorRed.yPrev-y)**2)**0.5;
-    
-    if (orangeClickDistance<25){
-        isOrangeCaptured = true;
-    } else if (greenClickDistance<25){
-        isGreenCaptured = true;
-    } else if (redClickDistance<25){
-        isRedCaptured = true;
+   
+    if (vectorOrange.clickDistance(x,y)<25){
+        vectorOrange.isCaptured = true;
+    } else if (vectorGreen.clickDistance(x,y)<25){
+        vectorGreen.isCaptured = true;
+    } else if (vectorRed.clickDistance(x,y)<25){
+        vectorRed.isCaptured = true;
     }
-
 }
 
 canvas.onmousemove = function(event){
     var x = event.offsetX;
     var y = event.offsetY;
-    var orangeClickDistance = ((vectorOrange.xPrev-x)**2+(vectorOrange.yPrev-y)**2)**0.5;
-    var greenClickDistance = ((vectorGreen.xPrev-x)**2+(vectorGreen.yPrev-y)**2)**0.5;
-    var redClickDistance = ((vectorRed.xPrev-x)**2+(vectorRed.yPrev-y)**2)**0.5;
-
+    
     // drawing directSequence if any vector is captured onmousedown and moved
-    if (isOrangeCaptured){
+    if (vectorOrange.isCaptured){
         directSequence.draw(x,y,4);
         console.log(`orange capture`);
-    } else if (isGreenCaptured) {
+    } else if (vectorGreen.isCaptured) {
         directSequence.draw(x,y,4);
         console.log(`green capture`);
-    } else if (isRedCaptured){
+    } else if (vectorRed.isCaptured){
         directSequence.draw(x,y,4);
         console.log(`red capture`);
     } else {
         // drawing directSequence if any vector is captured onmousemove
-        if (orangeClickDistance<25){
-            isOrangeLighted = true;
+        if (vectorOrange.clickDistance(x,y)<25){
+            vectorOrange.isLighted = true;
             directSequence.draw(vectorOrange.xPrev,vectorOrange.yPrev,4);
         }
         else{
-            if(isOrangeLighted){
+            if(vectorOrange.isLighted){
                 directSequence.draw(vectorOrange.xPrev,vectorOrange.yPrev);
             }
-            isOrangeLighted = false;
+            vectorOrange.isLighted = false;
         }
-        if (greenClickDistance<25){
-            isGreenLighted = true;
+        if (vectorGreen.clickDistance(x,y)<25){
+            vectorGreen.isLighted = true;
             directSequence.draw(vectorGreen.xPrev,vectorGreen.yPrev,4);
         }  
         else{
-            if (isGreenLighted){
+            if (vectorGreen.isLighted){
                 directSequence.draw(vectorGreen.xPrev,vectorGreen.yPrev);
             }
-            isGreenLighted = false;
+            vectorGreen.isLighted = false;
         } 
 
-        if (redClickDistance<25){
-            isRedLighted = true;
+        if (vectorRed.clickDistance(x,y)<25){
+            vectorRed.isLighted = true;
             directSequence.draw(vectorRed.xPrev,vectorRed.yPrev,4);
         }
         else {
-            if (isRedLighted){
+            if (vectorRed.isLighted){
                 directSequence.draw(vectorRed.xPrev,vectorRed.yPrev);
             }
-            isRedLighted = false;
+            vectorRed.isLighted = false;
         }
     }
+    document.getElementById("Va").value = vectorOrange.radius();
+    document.getElementById("Vb").value = vectorGreen.radius();
+    document.getElementById("Vc").value = vectorRed.radius();
+    document.getElementById("FIa").value = vectorOrange.xPrev;
+    document.getElementById("FIb").value = vectorOrange.xPrev;
+    document.getElementById("FIc").value = vectorOrange.xPrev;
 }
 
 canvas.onmouseup = function(event){
-    isRedCaptured = false;
-    isGreenCaptured = false;
-    isOrangeCaptured = false;
+    vectorRed.isCaptured = vectorGreen.isCaptured = vectorOrange.isCaptured = false;
 }
 
 canvas.onmouseout = function(event){
-    isRedCaptured = false;
-    isGreenCaptured = false;
-    isOrangeCaptured = false;
+    vectorRed.isCaptured = vectorGreen.isCaptured = vectorOrange.isCaptured = false;
 }
 
-isOrangeCaptured = true;
+vectorOrange.isCaptured = true;
 directSequence.draw(initXOrange,initYOrange);
-isOrangeCaptured = false;
+vectorOrange.isCaptured = false;
 
-
+// init fields
+document.getElementById("Va").value = vectorOrange.radius();
+document.getElementById("Vb").value = vectorGreen.radius();
+document.getElementById("Vc").value = vectorRed.radius();
+document.getElementById("FIa").value = vectorOrange.xPrev;
+document.getElementById("FIb").value = vectorOrange.xPrev;
+document.getElementById("FIc").value = vectorOrange.xPrev;
